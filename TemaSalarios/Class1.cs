0eeac8c97f28fb2e1e035eba66de109d1db46ba8
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-11-18 21:21:43 -0300

    codigo finalizado

---
+++ TemaSalarios/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemaSalarios
+{
+    class Salario
+    {
+        // Variáveis
+        private int _id;
+        private string _nome;
+        private double _salario;
+        public Salario(int id, string nome, double salario) // Construtor
+        {
+            _id = id;
+            _nome = nome;
+            _salario = salario;
+        }
+        public void aumentarSalario(double porcentagem) // Altera o salário baseado na porcentagem
+        {
+            _salario += (_salario * porcentagem / 100);
+        }
+        public int ID // Properties do _id
+        {
+            get { return _id; }
+        }
+        public string Nome // Properties do _nome
+        {
+            get { return _nome; }
+        }
+        public double Pagamento // Properties do _salario
+        {
+            get { return _salario; }
+        }
+    }
+}
--- TemaSalarios/Program.cs
+++ TemaSalarios/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace TemaSalarios
 {
@@ -6,7 +8,40 @@ namespace TemaSalarios
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Salario> funcionario = new List<Salario>(); // Lista dos funcionários
+            Console.Write("Quantos funcionários serão registrados? "); // Entrada do número de funcionários
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) // Repetição para cada funcionário
+            {
+                Console.WriteLine("Funcionário #" + i + ":");
+                Console.Write("Id: "); // Id do usuário
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: "); // Nome do usuário
+                string nome = Console.ReadLine();
+                Console.Write("Salário: "); // Salário do usuário
+                double salario = double.Parse(Console.ReadLine());
+                Console.WriteLine("--------------------------------------------");
+                Salario s = new Salario(id, nome, salario);
+                funcionario.Add(s);
+            }
+            Console.Write("\nId do funcionário que receberá aumento: "); // Verá qual funcionário irá receber aumento
+            int Id = int.Parse(Console.ReadLine());
+            Salario f = funcionario.Find(x => x.ID == Id); // Procurar o funcionário na lista
+            if (f != null) // Ver se o funcionário existe
+            {
+                Console.Write("Porcentagem de aumento de salário: "); // Realizar o aumento de salário
+                f.aumentarSalario(double.Parse(Console.ReadLine())); 
+            }
+            else // Funcionario inexistente
+            {
+                Console.WriteLine("Este funcionário não existe!");
+            }
+            Console.WriteLine("\nLista atualizada dos funcionários:"); // Lista atualizada
+            foreach (Salario obj in funcionario)
+            {
+                Console.WriteLine(obj.ID.ToString("D3") + ", " + obj.Nome + ", " + obj.Pagamento.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            Console.ReadKey();
         }
     }
 }
